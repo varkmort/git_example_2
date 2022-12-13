a7c53830884847093536217637342e192ed80871
Vladislav Tsibart <varkmort@gmail.com>
2022-12-13 19:17:59 +0300

    Добавьте файлы проекта.

---
+++ example/example leap year.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	using namespace std;
+	cout << "Введите год: ";
+	int year{};
+	cin >> year;
+	if (((!(year%4) && year%100 ) || !(year%400)) && year%1000){
+
+		cout << year << " - високосный год\n";
+	}
+	else {
+		cout << year << " - не високосный год\n";
+	}
+
+	return 0;
+}
